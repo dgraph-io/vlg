0186a85bf262d407e3e29df29df8c9f343febcc0
mattthew <matthew.mcneely@gmail.com>
2023-03-16 17:18:21 -0400

    Add func to count relationships by type

--- tools/model/relationship.go
+++ tools/model/relationship.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/timshannon/badgerhold/v4"
+)
+
 type Relationship struct {
 	FromID           int64    `csv:"node_id_start"`
 	FromType         string   `csv:"-"`
@@ -10,4 +17,35 @@ type Relationship struct {
 	StartDate        DateTime `csv:"start_date"`
 	EndDate          DateTime `csv:"end_date"`
 	SourceID         string   `csv:"source_id"`
+}
+
+func CountRelationships(store *badgerhold.Store) string {
+	var result strings.Builder
+	var missingCount int
+	q := &badgerhold.Query{}
+	relMap := make(map[string]int)
+	tx := store.Badger().NewTransaction(true)
+	defer tx.Discard()
+	err := store.ForEach(q, func(record *Relationship) error {
+		if record.FromType == "" || record.ToType == "" {
+			missingCount++
+			return nil
+		}
+		key := record.FromType + "->" + record.ToType + "->" + record.RelationshipType
+		_, ok := relMap[key]
+		if !ok {
+			relMap[key] = 1
+		} else {
+			relMap[key]++
+		}
+		return nil
+	})
+	if err != nil {
+		result.WriteString(err.Error())
+	}
+	result.WriteString(fmt.Sprintf("Missing: %d\n", missingCount))
+	for k, v := range relMap {
+		result.WriteString(fmt.Sprintf("%s: %d\n", k, v))
+	}
+	return result.String()
 }
