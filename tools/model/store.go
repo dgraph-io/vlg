748eddc6c8f1c86973dc0c5c48b3c5bd2c1cdd60
mattthew <matthew.mcneely@gmail.com>
2023-03-27 15:58:17 -0400

    Add comment

--- tools/model/store.go
+++ tools/model/store.go
@@ -4,6 +4,7 @@ import "github.com/timshannon/badgerhold/v4"
 
 const dataDirectory = "data"
 
+// GetStore returns a badgerhold store
 func GetStore(readonly bool) (*badgerhold.Store, error) {
 	options := badgerhold.DefaultOptions
 	options.Dir = dataDirectory
