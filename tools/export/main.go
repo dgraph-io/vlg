2824014f54c6039426019472ffa71ee9ad38d69b
mattthew <matthew.mcneely@gmail.com>
2023-04-11 14:16:27 -0400

    Add relationship exporting

--- tools/export/main.go
+++ tools/export/main.go
@@ -4,7 +4,9 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/timshannon/badgerhold/v4"
 
 	"vlg/tools/model"
@@ -15,6 +17,7 @@ const exportDir = "../rdf"
 // exports all records to RDF files.
 func main() {
 
+	now := time.Now()
 	store, err := model.GetStore(true)
 	defer func() {
 		_ = store.Close()
@@ -35,14 +38,38 @@ func main() {
 		log.Println("Exporting", k)
 		f, err := os.Create(exportDir + "/" + k)
 		if err != nil {
-			panic(err)
+			panic(errors.Wrapf(err, "creating file %s", k))
 		}
 		defer func() {
-			_ = f.Close()
+			if err = f.Close(); err != nil {
+				log.Println(errors.Wrapf(err, "closing file %s", f.Name()))
+			}
 		}()
 		err = v(f, store)
 		if err != nil {
-			panic(err)
+			panic(errors.Wrapf(err, "exporting %s", k))
 		}
 	}
+
+	// load all relationships
+	var relationships []*model.Relationship
+	err = store.Find(&relationships, &badgerhold.Query{})
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Exporting", len(relationships), "relationships")
+	f, err := os.Create(exportDir + "/relationships.rdf")
+	if err != nil {
+		panic(errors.Wrap(err, "creating file relationships.rdf"))
+	}
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Println(errors.Wrapf(err, "closing file %s", f.Name()))
+		}
+	}()
+	for i := range relationships {
+		relationships[i].ToRDF(f)
+	}
+
+	log.Println("Exported all records in", time.Since(now))
 }
