dee4a0c2c075914fe0c2c0dba71733e6bb53ace5
mattthew <matthew.mcneely@gmail.com>
2023-04-26 17:41:08 -0400

    Add some notables to the export; skip dup random indexes

--- tools/export-subset/main.go
+++ tools/export-subset/main.go
@@ -30,7 +30,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	count := len(relationships)
 	f, err := os.Create(exportDir + "/data.rdf")
 	if err != nil {
 		panic(err)
@@ -39,10 +38,17 @@ func main() {
 		_ = f.Close()
 	}()
 
+	count := len(relationships)
+	relationshipSet := make(map[int]struct{})
 	// randomly select relationships (note: seed hard-coded for reproducibility)
 	rand.Seed(42)
 	for n := 0; n < exportCount; n++ {
 		i := rand.Intn(int(count))
+		if _, ok := relationshipSet[i]; ok {
+			n-- // try again
+			continue
+		}
+		relationshipSet[i] = struct{}{}
 		from, _, err := model.RecordByID(store, relationships[i].FromID)
 		if err != nil {
 			panic(err)
@@ -54,8 +60,38 @@ func main() {
 		from.ToRDF(f)
 		to.ToRDF(f)
 		relationships[i].ToRDF(f)
-		if n%1000 == 0 {
-			log.Printf("Exported %d relationships of %d", n, exportCount)
+		if n%10000 == 0 {
+			log.Printf("Exporting %d of %d relationships ...", n, exportCount)
 		}
 	}
+	log.Println("Exported", len(relationshipSet), "random relationships")
+	// load some notable records
+	notables := map[string]struct{}{
+		"182471":   {}, // Imee Marcos: The daughter of former Philippine dictator Ferdinand Marcos
+		"80133535": {}, // Rex Tillerson: Former ExxonMobil CEO and U.S. Secretary of State
+		"80118878": {}, // Wilbur Ross: Former U.S. Secretary of Commerce
+		"84100003": {}, // The Duchy of Lancaster: The private estate of the British monarch
+		"56073968": {}, // Shakira: Colombian singer
+	}
+	notableCount := 0
+	for i := range relationships {
+		r := relationships[i]
+		_, fromFound := notables[r.FromID]
+		_, toFound := notables[r.ToID]
+		if toFound || fromFound {
+			from, _, err := model.RecordByID(store, r.FromID)
+			if err != nil {
+				panic(err)
+			}
+			to, _, err := model.RecordByID(store, r.ToID)
+			if err != nil {
+				panic(err)
+			}
+			from.ToRDF(f)
+			to.ToRDF(f)
+			r.ToRDF(f)
+			notableCount++
+		}
+	}
+	log.Printf("Exported %d notable relationships", notableCount)
 }
